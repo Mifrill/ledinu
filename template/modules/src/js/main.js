03db34c107c60c570ad217e6d3163a334f4a9107
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-06-26 09:27:46 +0600

    cleanup

--- template/modules/src/js/main.js
+++ template/modules/src/js/main.js
@@ -1,27 +1,27 @@
 /* Russian (UTF-8) initialisation for the jQuery UI date picker plugin. */
 ( function( factory ) {
 
-	factory( jQuery.datepicker );
+  factory( jQuery.datepicker );
 
 }( function( datepicker ) {
 
 datepicker.regional.ru = {
-	closeText: "Закрыть",
-	prevText: "&#x3C;Пред",
-	nextText: "След&#x3E;",
-	monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь",
-	"Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
-	monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
-	"Июл","Авг","Сен","Окт","Ноя","Дек" ],
-	dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
-	dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
-	dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
-	weekHeader: "Нед",
-	dateFormat: "dd.mm.yy",
-	firstDay: 1,
-	isRTL: false,
-	showMonthAfterYear: false,
-	yearSuffix: "" };
+  closeText: "Закрыть",
+  prevText: "&#x3C;Пред",
+  nextText: "След&#x3E;",
+  monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь",
+  "Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
+  monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
+  "Июл","Авг","Сен","Окт","Ноя","Дек" ],
+  dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
+  dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
+  dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
+  weekHeader: "Нед",
+  dateFormat: "dd.mm.yy",
+  firstDay: 1,
+  isRTL: false,
+  showMonthAfterYear: false,
+  yearSuffix: "" };
 datepicker.setDefaults( datepicker.regional.ru );
 
 return datepicker.regional.ru;
@@ -29,43 +29,43 @@ return datepicker.regional.ru;
 } ) );
 
 $('#sl-video').slick({
-	dots: true,
-	autoplay: true,
-	autoplaySpeed: 7000,
-	arrows: false,
-	infinite: true,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 7000,
+  arrows: false,
+  infinite: true,
 });
 
 
 $(".container").on("click","a:not(#tel-call-id)", function (event) {
-	event.preventDefault();
-	var id  = $(this).attr('href'),
-		top = $(id).offset().top;
-	$('body,html').animate({scrollTop: top}, 1500);
-	$('#scroll-menu>li').removeClass('active');
-	$(this.closest('li')).addClass('active');
+  event.preventDefault();
+  var id  = $(this).attr('href'),
+    top = $(id).offset().top;
+  $('body,html').animate({scrollTop: top}, 1500);
+  $('#scroll-menu>li').removeClass('active');
+  $(this.closest('li')).addClass('active');
 });
 
 $("#scroll-menu>li>a").click(function(){
-	$('.navbar-collapse').removeClass('in');
+  $('.navbar-collapse').removeClass('in');
 })
 
 $(function(){
   $(document).click(function(event) {
-	if ($(event.target).closest("#scroll-menu>li>a").length) return;
-	$('.navbar-collapse').removeClass('in');
-	event.stopPropagation();
+  if ($(event.target).closest("#scroll-menu>li>a").length) return;
+  $('.navbar-collapse').removeClass('in');
+  event.stopPropagation();
   });
 });
 
 /*
 
 $('.button-order').click(function(){
-	$('button[type="submit"]').attr('disabled', false);
-	$("#form-proposal").trigger("reset");
-	$('.alert').remove();
-	$('button[type="submit"]').empty();
-	$('button[type="submit"]').append('Отправить заявку');
+  $('button[type="submit"]').attr('disabled', false);
+  $("#form-proposal").trigger("reset");
+  $('.alert').remove();
+  $('button[type="submit"]').empty();
+  $('button[type="submit"]').append('Отправить заявку');
 });
 
 */
